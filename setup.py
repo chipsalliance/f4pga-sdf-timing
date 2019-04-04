d276efdfc8cf2f978a44a52fd231e73aff69f9f4
Tim 'mithro' Ansell <me@mith.ro>
2019-04-04 16:25:49 -0700

    Adding Python module stuff.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdf_timing",
+    version="0.0.1",
+    author="SymbiFlow Authors",
+    author_email="symbiflow@lists.librecores.org",
+    description="Python library for working Standard Delay Format (SDF) Timing Annotation files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SymbiFlow/python-sdf-timing",
+    packages=setuptools.find_packages(),
+    install_requires=['ply', 'json'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['sdf_timing_parse=sdf_timing.sdfparse:main'],
+    },
+)
