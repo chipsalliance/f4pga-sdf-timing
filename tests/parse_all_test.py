94f8d70900f72e48ae169a0056441b9c4aebf33e
Karol Gugala <kgugala@antmicro.com>
2019-05-16 22:53:51 +0200

    tests: add output stability test
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- tests/parse_all_test.py
+++ tests/parse_all_test.py
@@ -3,13 +3,14 @@ import os
 
 
 datafiles_path = 'tests/data/'
+goldenfiles_path = 'tests/data/golden/'
 parsed_sdfs = list()
 generated_sdfs = list()
 
 
 def test_parse():
     files = os.listdir(datafiles_path)
-    for f in files:
+    for f in sorted(files):
         if f.endswith('.sdf'):
             with open(datafiles_path + f) as sdffile:
                 parsed_sdfs.append(sdfparse.parse(sdffile.read()))
@@ -18,6 +19,23 @@ def test_parse():
 def test_emit():
     for s in parsed_sdfs:
         generated_sdfs.append(sdfparse.emit(s))
+
+
+def test_output_stability():
+    """ This test checks if the generated sdf are
+        identical with golde sdfs"""
+
+    parsed_sdfs_check = list()
+    # read the golden files
+    files = os.listdir(goldenfiles_path)
+    for f in sorted(files):
+        if f.endswith('.sdf'):
+            with open(goldenfiles_path + f) as sdffile:
+                parsed_sdfs_check.append(sdffile.read())
+
+    for s0, s1 in zip(parsed_sdfs, parsed_sdfs_check):
+        sdf0 = sdfparse.emit(s0)
+        assert sdf0 == s1
 
 
 def test_parse_generated():
